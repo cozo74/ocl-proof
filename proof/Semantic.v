cefc0836d3e4e8f8f6332f70b025abe25a8efffe
cz <caozhuozz@163.com>
2025-12-23 09:38:28 +0800

    add binop step

--- proof/Semantic.v
+++ proof/Semantic.v
@@ -49,7 +49,7 @@ Inductive value : tm -> Prop :=
 
 Inductive step : obj_model -> env -> tm -> tm -> Prop :=
 
-      (* context C inv body 语义：对所有实例执行 forAll *)
+  (* context C inv body 语义：对所有实例执行 forAll *)
       | E_Context_tru :
           forall M E C body,
           E_Forall M E "self" body ((cmap M) C) (CBool true) ->
@@ -61,6 +61,7 @@ Inductive step : obj_model -> env -> tm -> tm -> Prop :=
             step M E (CContext C body) (CBool false)
       
 
+  (* Var Self *)
       | E_Var :
           forall M E var,
           step M E (CVar var) (E var)
@@ -69,6 +70,7 @@ Inductive step : obj_model -> env -> tm -> tm -> Prop :=
           forall M E,
           step M E CSelf (E "self")
 
+  (* 一元运算 *)
       
       | E_BoolUnUNotTru :
         forall M E,
@@ -116,8 +118,9 @@ Inductive step : obj_model -> env -> tm -> tm -> Prop :=
         forall M E s,
         step M E (CStrUn UToLower (CString s)) ( CString (toLower s) )
 
+  (* 二元运算 *)
 
-
+    (* 二元逻辑运算 *)
       | E_BoolBinBAnd : 
         forall M E a b,
         step M E (CBoolBin BAnd (CBool a) (CBool b)) (CBool (andb a b) ) 
@@ -134,7 +137,7 @@ Inductive step : obj_model -> env -> tm -> tm -> Prop :=
         forall M E a b,
         step M E (CBoolBin BImplies (CBool a) (CBool b)) (CBool (orb (negb a) b) ) 
 
-
+    (* 二元比较运算 *)
 
       | E_CompBinBEq :
         forall M E a b,
@@ -147,11 +150,195 @@ Inductive step : obj_model -> env -> tm -> tm -> Prop :=
 
       | E_CompBinBEq2 :
         forall M E a b,
-        step M E (CCompBin BEq (CReal a) (CReal b)) (CBool (Reqb a b))
+        step M E (CCompBin BEq (CReal a) (CReal b)) (CBool (Reqb a b) )
+
+      | E_CompBinBEq3 :
+        forall M E a b,
+        step M E (CCompBin BEq (CString a) (CString b)) (CBool (String.eqb a b) )
+
+      | E_CompBinBEq4 :
+        forall M E a b,
+        step M E (CCompBin BEq (CObject a) (CObject b)) (CBool (String.eqb a b) )
 
 
 
 
+      | E_CompBinBNe :
+        forall M E a b,
+        step M E (CCompBin BNe (CInt a) (CInt b)) (CBool (negb (a =? b)%Z))
+
+
+      | E_CompBinBNe1 :
+        forall M E a b,
+        step M E (CCompBin BNe (CInt a) (CReal b)) (CBool (Rneqb (IZR a) (b)) )
+
+      | E_CompBinBNe2 :
+        forall M E a b,
+        step M E (CCompBin BNe (CReal a) (CReal b)) (CBool (Rneqb a b) )
+
+
+      | E_CompBinBNe3 :
+        forall M E a b,
+        step M E (CCompBin BEq (CString a) (CString b)) (CBool (negb (String.eqb a b)) )
+
+
+
+      | E_CompBinBNe4 :
+        forall M E a b,
+        step M E (CCompBin BEq (CObject a) (CObject b)) (CBool (negb (String.eqb a b)) )
+
+
+
+
+      | E_CompBinBLt :
+        forall M E a b,
+        step M E (CCompBin BLt (CInt a) (CInt b)) (CBool (a <? b)%Z)
+
+
+      | E_CompBinBLt1 :
+        forall M E a b,
+        step M E (CCompBin BLt (CInt a) (CReal b)) (CBool (Rltb (IZR a) (b)) )
+
+      | E_CompBinBLt2 :
+        forall M E a b,
+        step M E (CCompBin BLt (CReal a) (CReal b)) (CBool (Rltb a b) )
+
+      | E_CompBinBLe :
+        forall M E a b,
+        step M E (CCompBin BLe (CInt a) (CInt b)) (CBool (a <=? b)%Z)
+
+
+      | E_CompBinBLe1 :
+        forall M E a b,
+        step M E (CCompBin BLe (CInt a) (CReal b)) (CBool (Rleb (IZR a) (b)) )
+
+      | E_CompBinBLe2 :
+        forall M E a b,
+        step M E (CCompBin BLe (CReal a) (CReal b)) (CBool (Rleb a b) )
+
+
+      | E_CompBinBGt :
+        forall M E a b,
+        step M E (CCompBin BGt (CInt a) (CInt b)) (CBool (a >? b)%Z)
+
+
+      | E_CompBinBGt1 :
+        forall M E a b,
+        step M E (CCompBin BGt (CInt a) (CReal b)) (CBool (Rgtb (IZR a) (b)) )
+
+      | E_CompBinBGt2 :
+        forall M E a b,
+        step M E (CCompBin BGt (CReal a) (CReal b)) (CBool (Rgtb a b) )
+
+
+      | E_CompBinBGe :
+        forall M E a b,
+        step M E (CCompBin BGe (CInt a) (CInt b)) (CBool (a >=? b)%Z)
+
+
+      | E_CompBinBGe1 :
+        forall M E a b,
+        step M E (CCompBin BGe (CInt a) (CReal b)) (CBool (Rgeb (IZR a) (b)) )
+
+      | E_CompBinBGe2 :
+        forall M E a b,
+        step M E (CCompBin BGe (CReal a) (CReal b)) (CBool (Rgeb a b) )
+
+
+    (* 二元算数运算 *)
+
+      | E_ArithBinBAdd :
+        forall M E a b,
+        step M E (CArithBin BAdd (CInt a) (CInt b)) (CInt (a + b)%Z)
+
+      | E_ArithBinBAdd1 :
+        forall M E a b,
+        step M E (CArithBin BAdd (CInt a) (CReal b)) (CReal (IZR a + b)%R)
+
+      | E_ArithBinBAdd2 :
+        forall M E a b,
+        step M E (CArithBin BAdd (CReal a) (CReal b)) (CReal (a + b)%R)
+
+
+      | E_ArithBinBSub :
+        forall M E a b,
+        step M E (CArithBin BSub (CInt a) (CInt b)) (CInt (a - b)%Z)
+
+      | E_ArithBinBSub1 :
+        forall M E a b,
+        step M E (CArithBin BSub (CInt a) (CReal b)) (CReal (IZR a - b)%R)
+
+      | E_ArithBinBSub2 :
+        forall M E a b,
+        step M E (CArithBin BSub (CReal a) (CReal b)) (CReal (a - b)%R)
+
+
+      | E_ArithBinBMul :
+        forall M E a b,
+        step M E (CArithBin BMul (CInt a) (CInt b)) (CInt (a * b)%Z)
+
+      | E_ArithBinBMul1 :
+        forall M E a b,
+        step M E (CArithBin BMul (CInt a) (CReal b)) (CReal (IZR a * b)%R)
+
+      | E_ArithBinBMul2 :
+        forall M E a b,
+        step M E (CArithBin BMul (CReal a) (CReal b)) (CReal (a * b)%R)
+
+
+      | E_ArithBinBDiv :
+        forall M E a b,
+        step M E (CArithBin BDiv (CInt a) (CInt b)) (CReal ((IZR a) / (IZR b))%R)
+
+      | E_ArithBinBDiv1 :
+        forall M E a b,
+        step M E (CArithBin BDiv (CInt a) (CReal b)) (CReal (IZR a / b)%R)
+
+      | E_ArithBinBDiv2 :
+        forall M E a b,
+        step M E (CArithBin BDiv (CReal a) (CReal b)) (CReal (a / b)%R)
+
+    (* 二元字符串运算 *)
+
+      | E_StrBinBConcat :
+        forall M E a b,
+        step M E (CStrBin BConcat (CString a) (CString b)) (CString (a ++ b))
+
+    (* 二元聚合运算 *)
+
+      | E_AggBinAggBMax :
+        forall M E a b,
+        step M E (CAggBin BMax (CInt a) (CInt b)) (CInt (Z.max a b))
+
+      | E_AggBinAggBMax1 :
+        forall M E a b,
+        step M E (CAggBin BMax (CReal a) (CReal b)) (CReal (Rmax a b))
+
+      | E_AggBinAggMin :
+        forall M E a b,
+        step M E (CAggBin BMin (CInt a) (CInt b)) (CInt (Z.min a b))
+
+      | E_AggBinAggMin1 :
+        forall M E a b,
+        step M E (CAggBin BMin (CReal a) (CReal b)) (CReal (Rmin a b))
+
+
+      | E_AggBinAggBMod :
+        forall M E a b,
+        step M E (CAggBin BMod (CInt a) (CInt b)) (CInt (Z.modulo a b))
+
+
+      | E_AggBinAggBDivInt :
+        forall M E a b,
+        step M E (CAggBin BDivInt (CInt a) (CInt b)) (CInt (a / b)%Z)
+
+
+
+
+
+
+
+        
 
       with E_Forall :
             obj_model -> env -> string -> tm ->
